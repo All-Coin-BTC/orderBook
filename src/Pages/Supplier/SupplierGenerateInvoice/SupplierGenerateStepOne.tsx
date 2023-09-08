f1fbb31fad06b5373374d304714eb64dcb78b952
DylanCodesStuff <dscriven16@gmail.com>
2023-09-08 13:34:16 -0400

    pushing to my free acc

--- src/Pages/Supplier/SupplierGenerateInvoice/SupplierGenerateStepOne.tsx
+++ src/Pages/Supplier/SupplierGenerateInvoice/SupplierGenerateStepOne.tsx
@@ -13,41 +13,73 @@ export default function SupplierGenerateStepOne() {
 
   const dispatch = useDispatch();
   return (
-    <div className="step-one-container text-start flex flex-col items-center justify-center">
-      <div className="enter-warehouse-details-container rounded-2xl p-4 px-8">
-        <div className="title text-2xl font-semibold text-center">Step 1</div>
-        <div className="subtitle text-lg font-light text-center">Enter Receiving Warehouse Details</div>
-        <div className="warehouse-field-one font-light flex justify-between items-center mt-4 gap-1">
-          <div>Warehouse Name</div>
-          <input
-            className="warehouse-input"
-            type="text"
-            value={name}
-            onChange={(e) => dispatch(updateWarehouseName(e.target.value))}
-          />
+    <div className="h-[87vh] text-start sm:flex flex-col items-center justify-center">
+      <div
+        className="sm:max-h-[500px] sm:w-auto w-full
+         m-auo bg-white flex items-start space-x-6
+       rounded-xl py-4 pl-8 pr-2 shadow-xl"
+      >
+        {/* form */}
+        <div
+          className="h-full sm:w-[40%] w-full px-6 flex flex-col font-extrabold 
+        space-y-8 justify-between"
+        >
+          {/* container */}
+          <div className="flex flex-col space-y-7">
+            <div>
+              <h1 className="mt-6 title text-2xl font-semibold">Step 1</h1>
+              <h3 className="subtitle text-[1rem] font-light">Enter Receiving Warehouse Details</h3>
+            </div>
+            <div className="font-light flex flex-col space-y-4 justify-between items-start gap-1">
+              {/* <div className="w-full flex flex-col space-y-3 items-start"> */}
+              {/* <h3 className="text-gray-700 text-[1.1rem] font-semibold">Warehouse Name</h3> */}
+              <input
+                className="text-[0.9rem] px-3 text-left border placeholder:text-gray-600 border-gray-400 bg-white shadow-sm"
+                type="text"
+                placeholder="Warehouse Name"
+                value={name}
+                onChange={(e) => dispatch(updateWarehouseName(e.target.value))}
+              />
+              {/* </div> */}
+              {/* <div className="warehouse-field-two font-light flex justify-between items-center mt-4 gap-1"> */}
+              {/* <div>Warehouse ID</div> */}
+              <input
+                className="text-[0.9rem] px-3 text-left border placeholder:text-gray-600 border-gray-400 bg-white shadow-sm"
+                placeholder="Warehouse ID"
+                type="text"
+                value={id}
+                onChange={(e) => dispatch(updateWarehouseId(e.target.value))}
+              />
+              {/* </div> */}
+              {/* <div className="warehouse-field-two font-light flex justify-between items-center mt-4 gap-1"> */}
+              {/* <div>Warehouse Street Address</div> */}
+              <input
+                className="text-[0.9rem] px-3 text-left border placeholder:text-gray-600 border-gray-400 bg-white shadow-sm"
+                placeholder="Ware Street Address"
+                type="text"
+                value={address}
+                onChange={(e) => dispatch(updateWarehouseAddress(e.target.value))}
+              />
+            </div>
+          </div>
+          <div className="justify- w-full pb-10">
+            <Link to="/supplier-generate-step-two">
+              <button
+                className="bg-gray-900 w-full shadow-xl text-[0.8rem]
+               text-gray-300 py-2 rounded-md hover:scale-105 transition-all ease-linear"
+              >
+                Next Step
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className="warehouse-field-two font-light flex justify-between items-center mt-4 gap-1">
-          <div>Warehouse ID</div>
-          <input
-            className="warehouse-input"
-            type="text"
-            value={id}
-            onChange={(e) => dispatch(updateWarehouseId(e.target.value))}
+        {/* image */}
+        <div className="sm:inline-flex hidden h-full w-full">
+          <img
+            src="https://cdn.leonardo.ai/users/bbc6548d-60b8-4166-9742-86b290ee2135/generations/efe68df2-8be0-468a-8dba-ee17e37fa455/3D_Animation_Style_shipment_taking_place_from_a_truck_to_the_w_3.jpg"
+            alt="WareHouse"
+            className="h-full w-[100%] rounded-md object-cover"
           />
-        </div>
-        <div className="warehouse-field-two font-light flex justify-between items-center mt-4 gap-1">
-          <div>Warehouse Street Address</div>
-          <input
-            className="warehouse-input"
-            type="text"
-            value={address}
-            onChange={(e) => dispatch(updateWarehouseAddress(e.target.value))}
-          />
-        </div>
-        <div className="warehouse-details-submit mt-8 flex justify-center items-center">
-          <Link to="/supplier-generate-step-two">
-            <button className=" warehouse-submit-btn px-8 py-4 rounded-2xl">Next Step</button>
-          </Link>
         </div>
       </div>
     </div>
